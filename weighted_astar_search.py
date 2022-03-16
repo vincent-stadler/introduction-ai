d4913964c234ec53d2b8f91dfbd2d3a873d1b6e6
vincent-stadler <vincent.stadler@uzh.ch>
2022-03-16 16:01:06 +0100

    Update weighted_astar_search.py
    implementation

--- weighted_astar_search.py
+++ weighted_astar_search.py
@@ -1,5 +1,3 @@
-from datetime import time
-
 import pancake_problem
 
 from pancake_problem import PancakeProblem
@@ -25,46 +23,39 @@ class WeightedAStarSearch(Search):
             return [p.initial_state], 0
 
         # enqueue initial state
-        frontier = PriorityQueue()  # store here 2 element tuples with value of the evaluation function f = g + w * h and its state
-        h = p.h(p.initial_state)  # g is 0 for initial state, therefore f= 0 + h
-        f = h
-        first_tup = (f, SearchNode(p.initial_state, None, g=0))  # initial state has path cost g=0
-        frontier.put(first_tup)  # frontier will be sorted after first entry of tuple
+        frontier = PriorityQueue()
+        first_tup = (0, SearchNode(p.initial_state, None,
+                                   0))  # (cost, node (comparison between nodes is implemented in the SearchNode class, LIFO))
+        frontier.put(first_tup)  # cost = heurestics of node.state and node.g
         self.generated += 1
-        reached = {p.initial_state: f}
+        reached = {p.initial_state: 0}
 
         while not frontier.empty():
             node = frontier.get()[-1]
             self.expanded += 1
 
             for action in p.actions(node.state):
-                succ, g = p.result(node.state, action)
-                parent_node = node.parent
-                path_cost = g
-                while parent_node is not None:
-                    path_cost = path_cost + parent_node.g
-                    parent_node = parent_node.parent
-                f = path_cost + self.w * p.h(node.state)  # evaluation function f
-                succ_node = SearchNode(succ, node, 1)  # every g = 1?
+                succ, cost = p.result(node.state, action)
+                new_g = node.g + cost
+                succ_node = SearchNode(succ, node, new_g)
                 # early goal test
                 if p.is_goal(succ):
-                    return self.extract_path(succ_node), f
+                    return self.extract_path(succ_node), new_g
 
                 # mark reached to avoid cycles
-                if succ not in reached.keys():
-                    reached[succ] = f  # add successor node in reached with value f
+                if succ not in reached.keys() or new_g < reached[succ]:
+                    # here the heuristic of the succ node and it's path cost are calculated
+                    # print('path cost', new_g, 'heuristics', p.h(succ) )
+                    heuristics_and_path_cost = new_g + self.w * p.h(succ)
+                    reached[succ] = new_g
                     # enqueue successor
-                    tup = (f, succ_node)
-                    frontier.put(tup)  # do immer problem wenn (f=3, SearchNode1) zweimol im Prio Queue dine isch den verglichts SearchNodes mitenand was nid möglich isch
+                    frontier.put(tuple((heuristics_and_path_cost, succ_node)))
                     self.generated += 1
-
-        # no solution found
-        return None, None
 
 
 if __name__ == "__main__":
     problem = pancake_problem.generate_random_problem(5)
     problem = PancakeProblem((1, 5, 6, 2, 4, 3))
     problem.dump()
-    astar = WeightedAStarSearch(problem, weight=1, print_statistics=True)
+    astar = WeightedAStarSearch(problem, 1, print_statistics=True)
     astar.run()
